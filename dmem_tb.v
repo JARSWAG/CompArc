0cb4c19d9c46b9de3cf048e702012ce479135ca6
vmullary <145676245+vmullary@users.noreply.github.com>
2023-11-09 09:10:44 -0500

    Update dmem_tb.v

--- dmem_tb.v
+++ dmem_tb.v
@@ -33,28 +33,99 @@ initial begin
     #10 reset = 0;
 	 
 	 address[12:10] = 0;
-	 address[9:0] = ;
+	 address[9:0] = 0;
+	 Din = 32'hA5A5A5A5;
+	 read_write = 1;
+    #10;
+	 
+	 address[12:10] = 0;
+	 address[9:0] = 0;
+	 Din = 32'hA5A5A5A5;
+	 read_write = 0;
     #10;
 
     address[12:10] = 1;
+	 address[9:0] = 4;
+	 Din = 32'h12345678;
+	 read_write = 1;
+    #10;
+	 
+	 address[12:10] = 1;
+	 address[9:0] = 4;
+	 Din = 32'h12345678;
+	 read_write = 0;
     #10;
 
     address[12:10] = 2;
+	 address[9:0] = 8;
+	 Din = 32'h15328054;
+	 read_write = 1;
+    #10;
+	 
+	 address[12:10] = 2;
+	 address[9:0] = 8;
+	 Din = 32'h15328054;
+	 read_write = 0;
     #10;
 
     address[12:10] = 3;
+	 address[9:0] = 12;
+	 Din = 32'h00100234;
+	 read_write = 1;
+    #10;
+	 
+	 address[12:10] = 3;
+	 address[9:0] = 12;
+	 Din = 32'h00100234;
+	 read_write = 0;
     #10;
 
     address[12:10] = 4;
+	 address[9:0] = 0;
+	 Din = 32'h53601518;
+	 read_write = 1;
+    #10;
+	 
+	 address[12:10] = 4;
+	 address[9:0] = 0;
+	 Din = 32'h53601518;
+	 read_write = 0;
     #10;
 	 
 	 address[12:10] = 5;
+	 address[9:0] = 4;
+	 Din = 32'h69420632;
+	 read_write = 1;
+    #10;
+	 
+	 address[12:10] = 5;
+	 address[9:0] = 4;
+	 Din = 32'h69420632;
+	 read_write = 0;
     #10;
 	 
 	 address[12:10] = 6;
+	 address[9:0] = 8;
+	 Din = 32'h1839uaHF;
+	 read_write = 1;
+    #10;
+	 
+	 address[12:10] = 6;
+	 address[9:0] = 8;
+	 Din = 32'h1839uaHF;
+	 read_write = 0;
     #10;
 	 
 	 address[12:10] = 7;
+	 address[9:0] = 12;
+	 Din = 32'h97319711;
+	 read_write = 1;
+	 #10;
+	 
+	 address[12:10] = 7;
+	 address[9:0] = 12;
+	 Din = 32'h97319711;
+	 read_write = 0;
     #10;
 
     $finish;
