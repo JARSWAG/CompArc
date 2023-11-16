e1296ac4a454dcc796748a0787b04dd335bbf20a
reespillik <145676645+reespillik@users.noreply.github.com>
2023-11-16 08:42:13 -0500

    Add files via upload

--- dmem_tb.v
+++ dmem_tb.v
@@ -2,133 +2,70 @@
 
 module dmem_tb;
 
-reg [12:0] address;
-reg read_write;
-reg reset;
-reg clock;
-reg dataOut;
+reg clock, reset, enable, write_enable, read_enable;
+reg [31:0] memory_address, data_in, data_out;
 
-DMEM_TopLevel uut (
-	 .data_in(data_in),
-    .address(address),
+dmem uut (
     .clock(clock),
-    .read_write(read_write),
-	 .reset(reset),
-	 .dataOut(dataOut),
+    .reset(reset),
+    .enable(enable),
+    .memory_address(memory_address),
+    .data_in(data_in),
+    .write_enable(write_enable),
+    .read_enable(read_enable),
+    .data_out(data_out)
 );
 
 always begin
-    #1 clock = ~clock;
+    #5 clock = ~clock;
 end
 
 initial begin
     $dumpfile("dmem_tb.vcd");
     $dumpvars(0, dmem_tb);
 
+    clock = 0;
+    reset = 1;
+    enable = 0;
+    write_enable = 0;
+    read_enable = 0;
+    memory_address = 0;
     data_in = 0;
-    read_write = 1;
-	 reset = 1;
-    address = 0;
-	 
+
+    // Reset the DMEM
     #10 reset = 0;
-	 
-	 address[12:10] = 0;
-	 address[9:0] = 0;
-	 Din = 32'hA5A5A5A5;
-	 read_write = 1;
-    #10;
-	 
-	 address[12:10] = 0;
-	 address[9:0] = 0;
-	 Din = 32'hA5A5A5A5;
-	 read_write = 0;
+    #10 reset = 1;
+
+    // Enable the DMEM and write data at address 0
+    enable = 1;
+    write_enable = 1;
+    memory_address = 0;
+    data_in = 32'h12345678;
     #10;
 
-    address[12:10] = 1;
-	 address[9:0] = 4;
-	 Din = 32'h12345678;
-	 read_write = 1;
-    #10;
-	 
-	 address[12:10] = 1;
-	 address[9:0] = 4;
-	 Din = 32'h12345678;
-	 read_write = 0;
+    // Read data at address 0
+    write_enable = 0;
+    read_enable = 1;
     #10;
 
-    address[12:10] = 2;
-	 address[9:0] = 8;
-	 Din = 32'h15328054;
-	 read_write = 1;
-    #10;
-	 
-	 address[12:10] = 2;
-	 address[9:0] = 8;
-	 Din = 32'h15328054;
-	 read_write = 0;
+    // Disable the DMEM
+    enable = 0;
     #10;
 
-    address[12:10] = 3;
-	 address[9:0] = 12;
-	 Din = 32'h00100234;
-	 read_write = 1;
-    #10;
-	 
-	 address[12:10] = 3;
-	 address[9:0] = 12;
-	 Din = 32'h00100234;
-	 read_write = 0;
+    // Enable the DMEM again and write data at address 4
+    enable = 1;
+    write_enable = 1;
+    memory_address = 4;
+    data_in = 32'h98765432;
     #10;
 
-    address[12:10] = 4;
-	 address[9:0] = 0;
-	 Din = 32'h53601518;
-	 read_write = 1;
-    #10;
-	 
-	 address[12:10] = 4;
-	 address[9:0] = 0;
-	 Din = 32'h53601518;
-	 read_write = 0;
-    #10;
-	 
-	 address[12:10] = 5;
-	 address[9:0] = 4;
-	 Din = 32'h69420632;
-	 read_write = 1;
-    #10;
-	 
-	 address[12:10] = 5;
-	 address[9:0] = 4;
-	 Din = 32'h69420632;
-	 read_write = 0;
-    #10;
-	 
-	 address[12:10] = 6;
-	 address[9:0] = 8;
-	 Din = 32'h18399137;
-	 read_write = 1;
-    #10;
-	 
-	 address[12:10] = 6;
-	 address[9:0] = 8;
-	 Din = 32'h18399137;
-	 read_write = 0;
-    #10;
-	 
-	 address[12:10] = 7;
-	 address[9:0] = 12;
-	 Din = 32'h97319711;
-	 read_write = 1;
-	 #10;
-	 
-	 address[12:10] = 7;
-	 address[9:0] = 12;
-	 Din = 32'h97319711;
-	 read_write = 0;
+    // Read data at address 4
+    write_enable = 0;
+    read_enable = 1;
     #10;
 
     $finish;
 end
 
-endmodule 
+
+endmodule
